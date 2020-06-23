528e3cc801ef16eb22d9344687474b993012fdbd
Roman Travnikov <travnikovrn@gmail.com>
2020-06-23 16:20:53 +0300

    Updates

---
+++ precache-manifest.0f9cfd0ecfc22d21a88fda60e5516b35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8492e5b1ac92cb03a0bf33690eac1d",
+    "url": "/binance-cryptocurrency-widget/index.html"
+  },
+  {
+    "revision": "5d2023722c1a83411a66",
+    "url": "/binance-cryptocurrency-widget/static/css/main.8de60091.chunk.css"
+  },
+  {
+    "revision": "2073bd3c571a1d3912da",
+    "url": "/binance-cryptocurrency-widget/static/js/2.baead1d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/binance-cryptocurrency-widget/static/js/2.baead1d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2023722c1a83411a66",
+    "url": "/binance-cryptocurrency-widget/static/js/main.b060b012.chunk.js"
+  },
+  {
+    "revision": "f78b46365319a283d73f",
+    "url": "/binance-cryptocurrency-widget/static/js/runtime-main.dd0abf62.js"
+  }
+]);
